985f112ee0eeb138a264eae2c2df171ff7a86855
5ain <145506559+5ain@users.noreply.github.com>
2023-09-22 19:16:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function index(){
+    window.location.href = 'index.html';
+}
+
+function about(){
+    window.location.href= 'about.html';
+}
+
+function portfolio(){
+    window.location.href= 'portfolio.html';
+}
